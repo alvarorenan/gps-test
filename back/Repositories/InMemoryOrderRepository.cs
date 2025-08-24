0fa68f1c7de4dc9921830496b7dfe510eeed248d
alvaro renan <alvaroca1544@gmail.com>
2025-08-24 02:48:10 -0300

    refactor: remove deprecated in-memory repository classes; replace with mocks for testing

--- back/Repositories/InMemoryOrderRepository.cs
+++
@@ -1,8 +0,0 @@
-using GpsTest.Models;
-
-namespace GpsTest.Repositories;
-
-public class InMemoryOrderRepository : InMemoryRepository<Order>, IOrderRepository
-{
-    public IEnumerable<Order> GetByStatus(OrderStatus status) => _store.Values.Where(o => o.Status == status);
-}
